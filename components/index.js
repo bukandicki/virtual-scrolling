17deb29e5e81bd57cf05fa1769cc2cb5e1b1cb08
bukandicki <bukandicki@gmail.com>
2023-12-22 01:18:41 +0700

    add styled component registry

---
+++ components/index.js
@@ -0,0 +1,6 @@
+"use client"
+
+import StyledRegistry from "./StyledRegistry/StyledRegistry.component"
+import VirtualList from "./VirtualList/VirtualList.component"
+
+export { StyledRegistry, VirtualList }
